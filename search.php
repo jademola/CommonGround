f759f68a19a9e39bc2937210dd86f1e4b15c9527
Jimi Ademola <jimi.ademola1@gmail.com>
2025-04-08 01:24:51 -0700

    Resolved scroll issues with search page

--- search.php
+++ search.php
@@ -26,7 +26,7 @@
 <?php include "header.php" ?>
 
     <!-- Main Content -->     
-    <div class="main-content" id="mainFeed">
+    <div class="main-content">
 
     <!-- Popular sidebar --> 
     <aside class="sidebar">
