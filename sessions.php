efcdb224ceb2f09583515a965dc1496da3eac4a0
Jimi Ademola <jimi.ademola1@gmail.com>
2025-04-10 05:22:37 -0700

    Corrected bug causing session to be lost

--- sessions.php
+++ sessions.php
@@ -1,3 +1,15 @@
 <?php
 session_start();
+
+if (!isset($_SESSION['loggedIn'])) {
+    $_SESSION['loggedIn'] = false;
+}
+
+if (!isset($_SESSION['userType'])) {
+    $_SESSION['userType'] = '';
+}
+
+if (!isset($_SESSION['username'])) {
+    $_SESSION['username'] = '';
+}
 ?>
